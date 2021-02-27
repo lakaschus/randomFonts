323860c7c7d1115bd2fa0c6a083cfeb645775f1a
Phillip Lakaschus <Philliplakaschus@gmail.com>
2021-02-27 15:02:49 +0100

    prepare for merge

--- __init__.py
+++ __init__.py
@@ -1,1 +1,17 @@
 # Author: Phillip Lakaschus
+
+import sys, os
+from aqt import mw
+from aqt.utils import showInfo
+from aqt.qt import *
+from anki.hooks import addHook
+import json
+
+from anki.storage import Collection # OK
+
+with open('config.json', 'r') as json_file:
+    json_data = json.load(json_file)
+
+PROFILE_HOME = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print(PROFILE_HOME)
+print(json_data["user"])
---
+++ todo.md
@@ -0,0 +1,5 @@
+Anki Project to do:
+
+- Füge Addon Menu Punkt hinzu:
+    -> Wähle Deck aus -> Wähle Feld aus -> Wähle Anzahl an Fonts aus -> Random Color
+- Funktioniert tensorflow in Anki?
